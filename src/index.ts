074d6b5a6ea1ec1b5a1698c95ce498ef853f0b27
Barış DEMIRCI <demirci.baris38@gmail.com>
2022-12-10 10:26:52 +0300

    ✨ Bookmark service

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { status, ThrowableRouter, withParams } from "itty-router-extras";
 import { supabaseClient } from "./libs/supabase";
+import { bookmarkRouter } from "./routers/bookmark";
 import { shortenRouter } from "./routers/shorten";
 import { storageRouter } from "./routers/storage";
 
@@ -10,11 +11,13 @@ declare global {
 	const PASSWORD: string;
 	const BASE_URL: string;
 	const MAX_FILE_SIZE: string;
+	const DISCORD_WEBHOOK_URL: string;
 }
 
 const router = ThrowableRouter()
-	.all("/storage/*", (req) => storageRouter.handle(req))
-	.all("/shorten/*", (req) => shortenRouter.handle(req))
+	.all("/storage/*", (req) => storageRouter(req as Request)) // ? Why is this needed? IDK but it doesn't accepts the request type
+	.all("/shorten/*", (req) => shortenRouter(req as Request))
+	.all("/bookmark/*", (req) => bookmarkRouter(req as Request))
 	.get("/", () => status(200, "Hello, world! (Main)"))
 	.get("/:code", withParams, async (req) => {
 		const code = req.params?.code as string;
@@ -28,8 +31,8 @@ const router = ThrowableRouter()
 
 		return Response.redirect(codeExists.url, 301);
 	})
-	.all("*", () => status(404, "not found"));
+	.all("*", () => status(404, "not found")).handle;
 
-addEventListener("fetch", (event) =>
-	event.respondWith(router.handle(event.request)),
+addEventListener("fetch", (event: FetchEvent) =>
+	event.respondWith(router(event.request)),
 );
